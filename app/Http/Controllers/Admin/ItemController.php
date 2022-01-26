b831d7f6348c54a6ca807829557d79967ac785cc
YHF239921 <88521737+YHF239921@users.noreply.github.com>
2022-01-26 09:41:34 +0900

    123.

---
+++ app/Http/Controllers/Admin/ItemController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Item;
+
+class ItemController extends Controller
+{
+    public function index()
+    {
+        return view('admin.item.index');
+    }
+
+    public function create()
+    {
+        return view('admin.item.create');
+    }
+
+    public function add(Request $request)
+    {
+        $posts = $request->all();
+        // dd($posts);
+        Item::create($posts);
+        return redirect()->route('admin.item.index');
+    }
+
+}   
